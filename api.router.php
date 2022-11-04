83e98807d34e2c6d963d1d3132e4b415175e8e9c
Leonardo200298 <leonardosanchez119@gmail.com>
2022-11-04 18:41:04 -0300

    Feat:first endpoint ready

--- api.router.php
+++ api.router.php
@@ -1,9 +1,15 @@
 <?php
-
-require_once './libs/Router.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once './libs/router.php';
+require_once './app/controllers/peliculaController.php';
 
 // crea el router
 $router = new Router();
 
 // defina la tabla de ruteo
-$router->addRoute;
+$router->addRoute('peliculas', 'GET', 'PeliculasController', 'obtenerTodasLasPeliculas');
+
+
+// ejecuta la ruta (sea cual sea)
+$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
