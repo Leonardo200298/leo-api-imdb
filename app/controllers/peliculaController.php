0019d7cc4df7bbeeec4d51bed61a080141ead0c0
Leonardo200298 <leonardosanchez119@gmail.com>
2022-11-04 18:40:14 -0300

    Feat:movieControllers functions ready

--- app/controllers/peliculaController.php
+++ app/controllers/peliculaController.php
@@ -2,12 +2,28 @@
 require_once './app/models/peliculaModel.php';
 require_once './app/views/peliculaView.php';
 
-class PeliculasController{
+class PeliculasController
+{
     private $model;
     private $view;
+    private $data;
+
 
     function __construct()
     {
-        
+        $this->model = new PeliculasModel();
+        $this->view = new PeliculasView();
+        // lee el body del request
+        $this->data = file_get_contents("php://input");
+    }
+    private function getData()
+    {
+        return json_decode($this->data);
+    }
+
+    public function obtenerTodasLasPeliculas($params = null)
+    {
+        $peliculas = $this->model->conseguirTodasLasPeliculas();
+        $this->view->respuesta($peliculas);
     }
 }
