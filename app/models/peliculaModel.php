a409bd722cbee0df5d47cd76b95f3bd658b02ced
Leonardo200298 <leonardosanchez119@gmail.com>
2022-11-04 18:39:01 -0300

    Feat:movieModel functions ready

--- app/models/peliculaModel.php
+++ app/models/peliculaModel.php
@@ -1,5 +1,17 @@
 <?php
 
 class PeliculasModel{
-    
+    private $db;
+
+    function __construct()
+    {
+        $this->db=new PDO('mysql:host=localhost;'.'dbname=db_peliculas_pe;charset=utf8', 'root', '');
+    }
+
+    public function conseguirTodasLasPeliculas(){
+        $query = $this->db->prepare('SELECT * FROM peliculas');
+        $query -> execute();
+        $peliculas = $query->fetchAll(PDO::FETCH_OBJ);
+        return $peliculas;
+    }
 }
